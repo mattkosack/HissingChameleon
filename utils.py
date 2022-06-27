9c60b3a1844e6b46528524f1b6eb64d0d96fabdc
Matthew Kosack <matthew.kosack@gmail.com>
2022-06-27 01:00:38 -0400

    heroku changes

--- utils.py
+++ utils.py
@@ -1,9 +1,1 @@
 import requests
-from bs4 import BeautifulSoup
-
-# get the html from a link
-response = requests.get('https://www.twitch.tv/paymoneywubby')
-soup = BeautifulSoup(response.text, 'html.parser')
-
-with open('test.html', 'w') as f:
-    f.write(soup.prettify())
