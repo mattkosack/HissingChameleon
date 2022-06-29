e0b5a88bad22eb4a584dc33aac2b95c14041fb1f
Matt Kosack <matthew.kosack@gmail.com>
2022-06-29 14:46:01 -0400

    add pronunciation

--- hissing_chameleon.py
+++ hissing_chameleon.py
@@ -5,24 +5,23 @@ import discord
 import requests
 
 client = discord.Client()
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    # if message.content.startswith('$hello'):
-    #     await message.channel.send('Hello!')
-
-    # if any(word in message.content for word in ['Luke', 'luke']):
     if re.search(r"[Ll][^ -~]*[Uu][^ -~]*[Kk][^ -~]*[Ee]", message.content):
         await message.channel.send('I miss Luke :\\(')
 
+    if message.content.startswith('%name'):
+        await message.channel.send('https://audio.pronouncekiwi.com/enNEW1/sukapon')
 
-client.run(token)
+if __name__=="__main__":
+    load_dotenv()
+    token = os.getenv('DISCORD_TOKEN')
+    client.run(token)
