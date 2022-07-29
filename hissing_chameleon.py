b24c16007a2c233614f96d21def0ee5903f36cc7
Matthew Kosack <matthew.kosack@gmail.com>
2022-07-29 16:36:11 -0400

    test function

--- hissing_chameleon.py
+++ hissing_chameleon.py
@@ -22,12 +22,16 @@ async def on_message(message):
     if re.search(r"[Ll][^ -~]*[Uu][^ -~]*[Kk][^ -~]*[Ee]", message.content):
         await message.channel.send('I miss Luke :\\(')
 
+@bot.command(name='test')
+async def ping(ctx):
+    await ctx.send('pong')
 
 @bot.command(name="name", help="Says bot name")
 async def name(ctx):
     # await message.channel.send('https://audio.pronouncekiwi.com/enNEW1/sukapon')
     embed = discord.Embed(
         title="Say my name", url="https://audio.pronouncekiwi.com/enNEW1/sukapon", description="Say it.")
+    print('saying name')
     await ctx.channel.send(embed=embed)
 
 
