a93b50ff673b306c5e69177c215f2b6e1fc4a0f9
Matthew Kosack <matthew.kosack@gmail.com>
2022-08-26 12:11:59 -0400

    fix issue in color_from_text

--- utils.py
+++ utils.py
@@ -7,9 +7,12 @@ def get_line(file_name):
     return random.choice(lines)
 
 def get_color_from_text(phrase):
-    """Get the first color that matches the phrase"""
+    """
+    Get the first color that matches the phrase.
+    TODO: The issue is this finds the first match, not the best match.
+    """
     with open('rgb.txt') as f:
         for line in f:
             line = f.readline()
-            if phrase in line.split():
+            if phrase in " ".join(line.split()[:-1]):
                 return line.split()[-1]
