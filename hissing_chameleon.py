e18f4efcfff00173dd39faaa0b1b4706790f975c
Matthew Kosack <matthew.kosack@gmail.com>
2022-09-17 23:30:13 -0400

    voice

--- hissing_chameleon.py
+++ hissing_chameleon.py
@@ -42,7 +42,7 @@ async def name(ctx):
         await ctx.channel.send(embed=embed)
     else:
         voice_client = await user_voice_channel.connect()
-        voice_client.play(discord.FFmpegPCMAudio('files/sukapon-sukapon.mp3'), after=lambda _: await voice_client.disconnect())
+        voice_client.play(discord.FFmpegPCMAudio('files/sukapon-sukapon.mp3'), after=lambda _: voice_client.disconnect())
 
 
 @bot.command(name="leave", help="Makes the bot leave the voice channel")
