2edebab75c027b3f2bcc07c95258f3d790f761ff
Matthew Kosack <matthew.kosack@gmail.com>
2022-10-18 20:35:12 -0400

    using uinput, but it didn't work

--- servers/bot_server.py
+++ servers/bot_server.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     load_dotenv()
     ip = os.getenv('SERVER_IP')
     port = os.getenv('SERVER_PORT')
-    data = {'input': 'a'}
+    data = {'input': 'start'}
     send_input(ip, port, data)
