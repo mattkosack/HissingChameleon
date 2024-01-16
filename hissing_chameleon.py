26e88f87c6f7012558e60c730682d30b9990cd4f
Matthew Kosack <matthew.kosack@gmail.com>
2024-01-16 01:30:10 -0500

    Ai

--- hissing_chameleon.py
+++ hissing_chameleon.py
@@ -216,8 +216,8 @@ async def say(ctx, voice=None, *, text=None):
     try:
         voice_client = await user_voice_channel.connect()
         voice_client.play(discord.FFmpegPCMAudio(file))
-        while voice_client.is_playing():
-            await asyncio.sleep(1)
+        # while voice_client.is_playing():
+        #     await asyncio.sleep(1)
         await asyncio.sleep(1)
         await voice_client.disconnect()
     except Exception as e:
